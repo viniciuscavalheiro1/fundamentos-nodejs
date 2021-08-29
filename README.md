2f8cd5647e5686f780689a6100e1aeb23f69bf5a
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2021-08-29 00:15:07 -0300

    chore: add project

---
+++ README.md
@@ -0,0 +1,12 @@
+## Aprendendo NodeJS criando uma aplicação back-end 💻
+
+### Aulas do curso Ignite da rocketseat 🚀
+
+⛏️ Ainda não sei criar documentações bonitas, mas estou me esforçando para ter uma documentação fácil de entender. 
+
+####Intalando API
+
+```
+yarn install 
+```
+
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/courses", (request, response) => {
+  response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 1", "Curso 5", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 1", "Curso 5", "Curso 3", "Curso 4.3"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  response.json(["Curso 1", "Curso 5", "Curso 3"]);
+});
+
+app.listen(3333);  
+
